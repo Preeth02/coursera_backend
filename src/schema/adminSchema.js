aefe2a658f47f561d0dbf2df2bf112e613137f61
Preeth02 <preethampreetham27@gmail.com>
2024-09-29 20:07:22 +0530

    add password hashing to admins and methods to generate refresh token and access token

--- src/schema/adminSchema.js
+++ src/schema/adminSchema.js
@@ -1,4 +1,6 @@
 import mongoose, { Mongoose, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const adminSchema = new Schema(
   {
@@ -33,5 +35,34 @@ const adminSchema = new Schema(
     timestamps: true,
   }
 );
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+adminSchema.methods.isPasswordCorrect = async function (password) {
+  //It returns true or false
+  return await bcrypt.compare(this.password, password);
+};
 
-export const Admin = mongoose.model("Admin", adminSchema)
+adminSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+    },
+    process.env.ACCESS_TOKEN_ADMIN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+adminSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+export const Admin = mongoose.model("Admin", adminSchema);
